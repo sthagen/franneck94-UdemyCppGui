51c951a33c213b8a89991cff0718201e47613a59
franneck94 <schaffranek.jan@web.de>
2023-04-06 16:44:01 +0200

    up

--- 2_ImGui/6_Paint/src/render.cpp
+++ 2_ImGui/6_Paint/src/render.cpp
@@ -1,7 +1,5 @@
 #include <cstdint>
 #include <fstream>
-#include <iostream>
-#include <string>
 #include <string_view>
 #include <vector>
 
@@ -13,24 +11,21 @@
 
 void WindowClass::Draw(std::string_view title)
 {
-    static bool open_save_popup = false;
-    static bool open_load_popup = false;
-
-    ImGui::Begin(title.data(),
-                 NULL,
-                 ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoMove);
-
-    const auto mouse_pos = ImGui::GetMousePos();
+    static constexpr auto flags = ImGuiWindowFlags_NoCollapse |
+                                  ImGuiWindowFlags_NoMove |
+                                  ImGuiWindowFlags_NoResize;
+    ImGui::Begin(title.data(), NULL, flags);
 
     if (ImGui::Button("Save"))
     {
-        open_save_popup = true;
+        ImGui::OpenPopup("Save Image");
     }
+
     ImGui::SameLine();
 
     if (ImGui::Button("Load"))
     {
-        open_load_popup = true;
+        ImGui::OpenPopup("Load Image");
     }
     ImGui::SameLine();
 
@@ -41,48 +36,35 @@ void WindowClass::Draw(std::string_view title)
 
     DrawColorButtons();
 
-    ImGui::Text("Draw Size");
+    ImGui::Text("Draw size");
     ImGui::SameLine();
-    ImGui::SliderFloat("##draw_size", &point_draw_size, 1.0f, 10.0f, "%.1f");
+    ImGui::SliderFloat("##drawSize", &pointDrawSize, 1.0F, 10.0F);
 
-    if (open_save_popup)
+    if (ImGui::BeginPopupModal("Save Image"))
     {
-        ImGui::OpenPopup("Save Image");
-    }
-    if (ImGui::BeginPopupModal("Save Image",
-                               nullptr,
-                               ImGuiWindowFlags_AlwaysAutoResize))
-    {
-        ImGui::Text("File extension must be .bin");
-        ImGui::InputText("Filename", filename_buffer, sizeof(filename_buffer));
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
         if (ImGui::Button("Save"))
         {
-            SaveToImageFile(filename_buffer);
-            open_save_popup = false;
+            SaveToImageFile(filenameBuffer);
             ImGui::CloseCurrentPopup();
         }
+
         ImGui::SameLine();
+
         if (ImGui::Button("Cancel"))
         {
-            open_save_popup = false;
             ImGui::CloseCurrentPopup();
         }
+
         ImGui::EndPopup();
     }
 
-    if (open_load_popup)
+    if (ImGui::BeginPopupModal("Load Image"))
     {
-        ImGui::OpenPopup("Load Image");
-    }
-    if (ImGui::BeginPopupModal("Load Image",
-                               nullptr,
-                               ImGuiWindowFlags_AlwaysAutoResize))
-    {
-        ImGui::InputText("Filename", filename_buffer, sizeof(filename_buffer));
+        ImGui::InputText("Filename", filenameBuffer, sizeof(filenameBuffer));
         if (ImGui::Button("Load"))
         {
-            LoadFromImageFile(filename_buffer);
-            open_load_popup = false;
+            LoadFromImageFile(filenameBuffer);
             ImGui::CloseCurrentPopup();
         }
 
@@ -90,126 +72,118 @@ void WindowClass::Draw(std::string_view title)
 
         if (ImGui::Button("Cancel"))
         {
-            open_load_popup = false;
             ImGui::CloseCurrentPopup();
         }
+
         ImGui::EndPopup();
     }
 
-    canvas_pos = ImGui::GetCursorScreenPos();
-    const auto button_size = ImVec2(canvas_size.x + border_thickness * 2,
-                                    canvas_size.y + border_thickness * 2);
-    ImGui::InvisibleButton("canvas", button_size);
+    canvasPos = ImGui::GetCursorScreenPos();
+    const auto border_thickness = 1.5F;
+    const auto button_size = ImVec2(canvasSize.x + 2.0F * border_thickness,
+                                    canvasSize.y + 2.0F * border_thickness);
+    ImGui::InvisibleButton("##canvas", button_size);
 
+    const auto mouse_pos = ImGui::GetMousePos();
     const auto is_mouse_hovering = ImGui::IsItemHovered();
-    if (is_mouse_hovering && ImGui::IsMouseDown(0))
+    if (is_mouse_hovering && ImGui::IsMouseDown(ImGuiMouseButton_Left))
     {
-        const auto point =
-            ImVec2(mouse_pos.x - canvas_pos.x - border_thickness,
-                   mouse_pos.y - canvas_pos.y - border_thickness);
+        const auto point = ImVec2(mouse_pos.x - canvasPos.x - border_thickness,
+                                  mouse_pos.y - canvasPos.y - border_thickness);
         points.push_back(
-            std::make_tuple(point, current_draw_color, point_draw_size));
+            std::make_tuple(point, currentDrawColor, pointDrawSize));
     }
 
     auto *draw_list = ImGui::GetWindowDrawList();
     for (const auto &[point, color, size] : points)
     {
         draw_list->AddCircleFilled(
-            ImVec2(canvas_pos.x + border_thickness + point.x,
-                   canvas_pos.y + border_thickness + point.y),
+            ImVec2(canvasPos.x + border_thickness + point.x,
+                   canvasPos.y + border_thickness + point.y),
             size,
             color,
             16);
     }
 
-    const auto border_min = ImVec2(canvas_pos.x, canvas_pos.y);
+    const auto border_min = canvasPos;
     const auto border_max =
-        ImVec2(canvas_pos.x + button_size.x - border_thickness,
-               canvas_pos.y + button_size.y - border_thickness);
+        ImVec2(canvasPos.x + button_size.x - border_thickness,
+               canvasPos.y + button_size.y - border_thickness);
     draw_list->AddRect(border_min,
                        border_max,
                        IM_COL32(255, 255, 255, 255),
-                       0.0f,
+                       0.0F,
                        ImDrawCornerFlags_All,
                        border_thickness);
 
     ImGui::End();
 }
 
-void WindowClass::ClearCanvas()
-{
-    points.clear();
-}
-
 void WindowClass::SaveToImageFile(std::string_view filename)
 {
-    std::ofstream file(filename.data(), std::ios::binary);
+    auto out = std::ofstream(filename.data(), std::ios::binary);
 
-    if (!file)
-    {
-        std::cerr << "Error: Could not open the file for writing!\n";
+    if (!out || !out.is_open())
         return;
-    }
 
-    auto point_count = points.size();
-    file.write(reinterpret_cast<char *>(&point_count), sizeof(point_count));
+    const auto point_count = points.size();
+    out.write(reinterpret_cast<const char *>(&point_count),
+              sizeof(point_count));
 
     for (const auto &[point, color, size] : points)
     {
-        file.write(reinterpret_cast<const char *>(&point), sizeof(point));
-        file.write(reinterpret_cast<const char *>(&color),
-                   sizeof(color));
-        file.write(reinterpret_cast<const char *>(&size), sizeof(size));
+        out.write(reinterpret_cast<const char *>(&point), sizeof(point));
+        out.write(reinterpret_cast<const char *>(&color), sizeof(color));
+        out.write(reinterpret_cast<const char *>(&size), sizeof(size));
     }
-    file.close();
+
+    out.close();
 }
 
 void WindowClass::LoadFromImageFile(std::string_view filename)
 {
-    auto file = std::ifstream(filename.data(), std::ios::binary);
+    auto in = std::ifstream(filename.data(), std::ios::binary);
 
-    if (!file)
-    {
-        std::cerr << "Error: Could not open the file for reading!\n";
+    if (!in || !in.is_open())
         return;
-    }
 
     auto point_count = std::size_t{};
-    file.read(reinterpret_cast<char *>(&point_count), sizeof(point_count));
+    in.read(reinterpret_cast<char *>(&point_count), sizeof(point_count));
 
     ClearCanvas();
     points.reserve(point_count);
 
-    for (std::uint32_t i = 0; i < point_count; ++i)
+    for (std::size_t i = 0; i < point_count; i++)
     {
         auto point = ImVec2{};
         auto color = ImColor{};
         auto size = float{};
 
-        file.read(reinterpret_cast<char *>(&point), sizeof(point));
-        file.read(reinterpret_cast<char *>(&color), sizeof(color));
-        file.read(reinterpret_cast<char *>(&size), sizeof(size));
+        in.read(reinterpret_cast<char *>(&point), sizeof(point));
+        in.read(reinterpret_cast<char *>(&color), sizeof(color));
+        in.read(reinterpret_cast<char *>(&size), sizeof(size));
 
         points.push_back(std::make_tuple(point, color, size));
     }
-    file.close();
+
+    in.close();
 }
 
 void WindowClass::DrawColorButtons()
 {
-    const auto selected_red = current_draw_color == ImColor(255, 0, 0);
-    const auto selected_green = current_draw_color == ImColor(0, 255, 0);
-    const auto selected_blue = current_draw_color == ImColor(0, 0, 255);
-    const auto selected_white = current_draw_color == ImColor(255, 255, 255);
+    const auto selected_red = currentDrawColor == ImColor(255, 0, 0);
+    const auto selected_green = currentDrawColor == ImColor(0, 255, 0);
+    const auto selected_blue = currentDrawColor == ImColor(0, 0, 255);
+    const auto selected_white = currentDrawColor == ImColor(255, 255, 255);
     const auto none_preset_color =
         !selected_red && !selected_green && !selected_blue && !selected_white;
 
-    constexpr auto orange = ImVec4(1.0f, 0.5f, 0.0f, 1.0f);
+    constexpr static auto orange = ImVec4(1.0F, 0.5F, 0.0F, 1.0F);
 
     if (selected_red)
         ImGui::PushStyleColor(ImGuiCol_Button, orange);
     if (ImGui::Button("Red"))
-        current_draw_color = ImColor(255, 0, 0);
+        currentDrawColor = ImColor(255, 0, 0);
     if (selected_red)
         ImGui::PopStyleColor();
 
@@ -218,7 +192,7 @@ void WindowClass::DrawColorButtons()
     if (selected_green)
         ImGui::PushStyleColor(ImGuiCol_Button, orange);
     if (ImGui::Button("Green"))
-        current_draw_color = ImColor(0, 255, 0);
+        currentDrawColor = ImColor(0, 255, 0);
     if (selected_green)
         ImGui::PopStyleColor();
 
@@ -227,7 +201,7 @@ void WindowClass::DrawColorButtons()
     if (selected_blue)
         ImGui::PushStyleColor(ImGuiCol_Button, orange);
     if (ImGui::Button("Blue"))
-        current_draw_color = ImColor(0, 0, 255);
+        currentDrawColor = ImColor(0, 0, 255);
     if (selected_blue)
         ImGui::PopStyleColor();
 
@@ -236,31 +210,30 @@ void WindowClass::DrawColorButtons()
     if (selected_white)
         ImGui::PushStyleColor(ImGuiCol_Button, orange);
     if (ImGui::Button("White"))
-        current_draw_color = ImColor(255, 255, 255);
+        currentDrawColor = ImColor(255, 255, 255);
     if (selected_white)
         ImGui::PopStyleColor();
 
     ImGui::SameLine();
 
-    bool open_color_picker_popup = false;
     if (none_preset_color)
         ImGui::PushStyleColor(ImGuiCol_Button, orange);
     if (ImGui::Button("Choose"))
-        open_color_picker_popup = true;
-    if (none_preset_color)
-        ImGui::PopStyleColor();
-
-    if (open_color_picker_popup)
         ImGui::OpenPopup("Color Picker");
-
     if (ImGui::BeginPopup("Color Picker"))
     {
-        ImGui::ColorPicker4("##picker",
-                            reinterpret_cast<float *>(&current_draw_color),
-                            ImGuiColorEditFlags_NoInputs);
+        ImGui::ColorPicker3("##picker", reinterpret_cast<float *>(&currentDrawColor));
         ImGui::EndPopup();
     }
+    if (none_preset_color)
+        ImGui::PopStyleColor();
 }
+
+void WindowClass::ClearCanvas()
+{
+    points.clear();
+}
+
 
 void render(WindowClass &window_class)
 {
--- 2_ImGui/6_Paint/src/render.hpp
+++ 2_ImGui/6_Paint/src/render.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cstdint>
 #include <string_view>
 #include <tuple>
 #include <vector>
@@ -14,39 +13,31 @@ public:
 
 public:
     WindowClass()
-        : canvas_size(ImVec2(static_cast<float>(num_rows),
-                             static_cast<float>(num_cols))),
-          canvas_pos(ImVec2{}){};
+        : points({}), canvasPos({}), currentDrawColor(ImColor(255, 255, 255)),
+          pointDrawSize(2.0F){};
+
     void Draw(std::string_view title);
 
 private:
     void SaveToImageFile(std::string_view filename);
     void LoadFromImageFile(std::string_view filename);
-
-    void AddPixel(const ImVec2 &pos,
-                  const ImColor &color,
-                  std::vector<uint8_t> &buffer);
-
     void DrawColorButtons();
-
     void ClearCanvas();
 
 private:
-    const std::uint32_t num_rows = 800;
-    const std::uint32_t num_cols = 600;
-    const std::uint32_t num_channels = 3;
-    ImVec2 canvas_size;
+    const std::uint32_t numRows = 800;
+    const std::uint32_t numCols = 600;
+    const std::uint32_t channels = 3;
+    const ImVec2 canvasSize =
+        ImVec2(static_cast<float>(numRows), static_cast<float>(numCols));
 
     std::vector<PointData> points;
+    ImVec2 canvasPos;
 
-    ImVec2 canvas_pos;
+    ImColor currentDrawColor;
+    float pointDrawSize;
 
-    ImColor current_draw_color = ImColor(255, 255, 255);
-    float point_draw_size = 2.0f;
-
-    char filename_buffer[128] = "test.bin";
-
-    float border_thickness = 1.5f;
+    char filenameBuffer[256] = "test.bin";
 };
 
 void render(WindowClass &window_class);
