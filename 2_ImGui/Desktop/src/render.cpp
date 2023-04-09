2f3d336bf7f4f06e77e524c0b224000ccd17e155
franneck94 <schaffranek.jan@web.de>
2023-04-09 10:24:12 +0200

    up

--- 2_ImGui/Desktop/src/render.cpp
+++ 2_ImGui/Desktop/src/render.cpp
@@ -9,20 +9,25 @@
 
 void WindowClass::Draw(std::string_view label)
 {
+    constexpr auto window_flags =
+        (ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
+         ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar |
+         ImGuiWindowFlags_NoScrollWithMouse);
+
     ImGui::SetNextWindowSize(ImVec2(1280.0F, 680.0F), ImGuiCond_Always);
     ImGui::SetNextWindowPos(ImVec2(0.0F, 0.0F), ImGuiCond_Always);
 
-    ImGui::Begin(label.data(), NULL, ImGuiWindowFlags_NoDecoration);
+    ImGui::Begin(label.data(), NULL, window_flags);
 
     for (auto &icon : icons)
         icon.Draw();
 
-    ImGui::SetNextWindowSize(ImVec2(0.0F, 680.0F), ImGuiCond_Always);
-    ImGui::SetNextWindowPos(ImVec2(1280.0F, 40.0F), ImGuiCond_Always);
+    ImGui::SetNextWindowPos(ImVec2(0.0F, 680.0F), ImGuiCond_Always);
+    ImGui::SetNextWindowSize(ImVec2(1280.0F, 40.0F), ImGuiCond_Always);
 
-    ImGui::Begin("Taskbar");
+    ImGui::Begin("Taskbar", NULL, window_flags);
 
-    if (ImGui::Button("All Icons", ImVec2(100.0F, 20.0F)))
+    if (ImGui::Button("All Icons", ImVec2(100, 20)))
     {
         ImGui::OpenPopup("My Programs");
     }
@@ -39,15 +44,19 @@ void WindowClass::ShowIconList()
     const auto selectable_height = ImGui::GetTextLineHeightWithSpacing();
     const auto popup_height = selectable_height * numIcons + 40.0F;
 
-    ImGui::SetNextWindowSize(ImVec2(0.0F, 680.0F - popup_height),
+    ImGui::SetNextWindowPos(ImVec2(0.0F, 680.0F - popup_height),
                              ImGuiCond_Always);
-    ImGui::SetNextWindowPos(ImVec2(200.0F, popup_height), ImGuiCond_Always);
+    ImGui::SetNextWindowSize(ImVec2(200.0F, popup_height), ImGuiCond_Always);
 
     if (ImGui::BeginPopupModal("My Programs"))
     {
         for (auto &icon : icons)
         {
-            ImGui::Text("%s", icon.label.data());
+            if (ImGui::Selectable(icon.label.data()))
+            {
+                icon.popupOpen = true;
+                ImGui::CloseCurrentPopup();
+            }
         }
 
         ImGui::EndPopup();
@@ -69,7 +78,7 @@ void WindowClass::Icon::Draw()
 
     ImGui::End();
 
-    if (clickCount >= 1)
+    if (clickCount >= 1 || popupOpen)
     {
         ImGui::OpenPopup(label_popup.data());
         clickCount = 0;
