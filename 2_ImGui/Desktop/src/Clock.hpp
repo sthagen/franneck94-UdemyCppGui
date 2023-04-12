fd7736636c8bbbf8f9deebecbfcc4c7432c4b3f5
Jan Schaffranek <jan.schaffranek@zf.com>
2023-04-12 12:37:56 +0200

    up

---
+++ 2_ImGui/Desktop/src/Clock.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <cstdint>
+#include <string_view>
+#include <tuple>
+
+#include "imgui.h"
+#include "implot.h"
+#include <fmt/format.h>
+
+
+class WindowClass
+{
+public:
+    constexpr static auto PI = 3.14159f;
+    constexpr static auto circleRadius = 250.0f;
+
+public:
+    void Draw(std::string_view label);
+
+    void DrawClockHand(const float radius,
+                       const float angle,
+                       const ImColor color);
+    void DrawHourStrokes();
+
+    std::tuple<float, float, float> GetTheta();
+
+private:
+    ImVec2 center;
+};
