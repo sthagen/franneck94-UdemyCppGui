3a75c7d7667309539a39a158fd61178c7f624c2b
franneck94 <schaffranek.jan@web.de>
2023-04-20 17:47:47 +0200

    up

--- 2_ImGui/Final/src/OtherTopics.cpp
+++ 2_ImGui/Final/src/OtherTopics.cpp
@@ -5,56 +5,10 @@
 
 #include <imgui.h>
 #include <implot.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <GLES2/gl2.h>
-#endif
-#include "lodepng.h"
-#include <GLFW/glfw3.h>
 #include <fmt/format.h>
 
 #include "OtherTopics.hpp"
-
-static std::tuple<GLuint, std::uint32_t, std::uint32_t> loadTexture(
-    const char *filename)
-{
-    std::vector<unsigned char> data;
-    std::uint32_t width, height;
-    const auto error = lodepng::decode(data, width, height, filename);
-
-    if (error)
-        throw "Error loading image";
-
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    glTexImage2D(GL_TEXTURE_2D,
-                 0,
-                 GL_RGBA,
-                 width,
-                 height,
-                 0,
-                 GL_RGBA,
-                 GL_UNSIGNED_BYTE,
-                 &data[0]);
-
-    return std::make_tuple(texture, width, height);
-}
-
-static void LoadAndDisplayImage(std::string_view image_filepath)
-{
-    const auto &[myImageTexture, imageWidth, imageHeight] =
-        loadTexture(image_filepath.data());
-    const auto imageSize =
-        ImVec2(static_cast<float>(imageWidth), static_cast<float>(imageHeight));
-    ImGui::Image(reinterpret_cast<ImTextureID>(myImageTexture), imageSize);
-}
+#include "UiHelpers.hpp"
 
 void OtherTopics::Draw(std::string_view label, bool *open)
 {
