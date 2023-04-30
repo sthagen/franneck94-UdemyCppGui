2018cbdebc6546c7b04fe4cd72a2f3b719d49933
franneck94 <schaffranek.jan@web.de>
2023-04-30 10:10:28 +0200

    up

--- 2_ImGui/1_HelloWorld/src/render.cpp
+++ 2_ImGui/1_HelloWorld/src/render.cpp
@@ -7,7 +7,16 @@
 
 void WindowClass::Draw(std::string_view label)
 {
-    ImGui::Begin(label.data());
+    constexpr static auto mainWindowFlags =
+        ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
+    constexpr static auto mainWindowSize = ImVec2(1280.0F, 720.0F);
+    constexpr static auto mainWindowPos = ImVec2(0.0F, 0.0F);
+
+    ImGui::SetNextWindowSize(ImVec2(1280.0F, 720.0F));
+    ImGui::SetNextWindowPos(ImVec2(0.0F, 0.0F));
+
+    ImGui::Begin(label.data(), nullptr, mainWindowFlags);
 
 
     ImGui::End();
